b46c2032858a7df63ae908c1cddf53bd405b6ef3
Martino Visintin <martino.visintin@ovoenergy.com>
2018-10-12 16:49:01 +0100

    update deps

--- project.clj
+++ project.clj
@@ -4,15 +4,15 @@
   :license {:name "Eclipse Public License"
             :url  "http://www.eclipse.org/legal/epl-v10.html"}
 
-  :dependencies [[integrant "0.6.3"]
+  :dependencies [[integrant "0.7.0"]
                  [org.apache.kafka/kafka-clients "1.1.1" :exclusions [org.scala-lang/scala-library]]
                  [org.clojure/clojure "1.9.0"]
-                 [org.clojure/spec.alpha "0.2.168"]
+                 [org.clojure/spec.alpha "0.2.176"]
                  [org.clojure/tools.logging "0.4.1"]
-                 [ovotech/kafka-avro-confluent "0.11.0"]]
+                 [ovotech/kafka-avro-confluent "1.1.1-1"]]
 
   :profiles {:dev {:dependencies   [[vise890/multistub "0.1.1"]
-                                    [ovotech/kafka-clj-test-utils "0.2.0-1"]
+                                    [ovotech/kafka-clj-test-utils "1.1.1-1"]
                                     [ch.qos.logback/logback-classic "1.2.3"]
                                     [ch.qos.logback/logback-core "1.2.3"]]
                    :resource-paths ["dev/resources"]}
