62184b94e905daa74007effece893be117003a96
Martino Visintin <martino.visintin@ovoenergy.com>
2018-10-15 16:03:22 +0100

    Revert "traverse less, use newer kac"
    This reverts commit e11dcadc8d169ae059d68053387995327a85e373.

--- src/kafka_clj_utils/producers.clj
+++ src/kafka_clj_utils/producers.clj
@@ -59,10 +59,11 @@
     ;; NOTE Allow `_` in keys:
     ;; https://github.com/damballa/abracad#basic-deserialization
     (with-bindings {#'abracad.avro.util/*mangle-names* false}
-      (doseq [r records
-              :let  [k-key (get-in k-val [:metadata :eventId])
-                     k-val {:schema schema
-                            :record r}
+      (doseq [k-val (map (fn [v]
+                           {:schema avro-schema
+                            :value  v})
+                         records)
+              :let  [k-key (get-in k-val [:value :metadata :eventId])
                      failure-cbk (FailureTrackingCallback failure k-val)]]
         (.send k-producer
                (ProducerRecord. topic-name k-key k-val)
